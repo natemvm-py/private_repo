d6487f42d0b8f4a7351a67d01ea1e47f667538de
Nate <natecowlet2014@gmail.com>
2021-06-27 12:31:06 +1200

    commit

--- python/practicepythonorg/8 - rock paper scissors.py
+++ python/practicepythonorg/8 - rock paper scissors.py
@@ -1,5 +1,7 @@
 #TEST
 
+#fuck you
+
 #Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
 #Remember the rules:
 #Rock beats scissors
