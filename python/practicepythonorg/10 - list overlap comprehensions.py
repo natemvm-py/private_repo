e1a148459c326fe497dbd93e29389e3c6ac0f64b
natemvm-py <84882038+natemvm-py@users.noreply.github.com>
2021-06-28 11:21:30 +1200

    Add files via upload

---
+++ python/practicepythonorg/10 - list overlap comprehensions.py
@@ -0,0 +1,28 @@
+#This week’s exercise is going to be revisiting an old exercise (see Exercise 5), except require the solution in a different way.
+#Take two lists, say for example these two:
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+#and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes. Write this using at least one list comprehension. (Hint: Remember list comprehensions from Exercise 7)
+#Extra:
+#Randomly generate two lists to test this
+
+import random
+
+a = []
+b = []
+c = []
+
+for i in range(random.randint(5,15)):
+	a.append(random.randint(1,99))
+
+for i in range(random.randint(5,15)):
+	b.append(random.randint(1,99))
+
+print(a)
+print(b)
+
+for i in a:
+	if i in b:
+		c.append(i)
+		
+print('Common Numbers Found:', c)
