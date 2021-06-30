ae9bd0fad6c2f081982ce91d23c074a76ff74985
natemvm-py <84882038+natemvm-py@users.noreply.github.com>
2021-07-01 07:28:54 +1200

    Add files via upload

---
+++ python/practicepythonorg/17 - decode a web page.py
@@ -0,0 +1,20 @@
+#Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the New York Times homepage.
+
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.nytimes.com/'
+r = requests.get(url)
+r_html = r.text
+
+soup = BeautifulSoup(r_html, features='html.parser')
+
+articleNames = soup.find_all('h3',  {'class':'css-xxaj7r e1lsht870'})
+
+count = 1
+
+for i in articleNames:
+	print(count, i.text)
+	count += 1
+	
+#print(articleNames)
