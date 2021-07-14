77aea84f2d66859fbb436900ae16460a15563773
natemvm-py <natecowley2014@gmail.com>
2021-07-14 18:05:40 +1200

    a

---
+++ html&css/portfolio/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function()){
+  $(window).on('scroll',function()){
+    var scroll = $(window).scrollTop();
+    if(scroll>=50){
+      $("sticky").addClass("stickyadd")
+    }else{
+      $("sticky").removeClass("stickyadd")
+    }
+  }
+}
